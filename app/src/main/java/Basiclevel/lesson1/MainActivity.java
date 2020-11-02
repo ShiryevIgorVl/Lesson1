1b46e9f8d8db72ad716f090c15eb83182ee1f872
Igor <ishirayev@gmail.com>
2020-11-02 11:13:39 +0300

    02.11.20

--- app/src/main/java/Basiclevel/lesson1/MainActivity.java
+++ app/src/main/java/Basiclevel/lesson1/MainActivity.java
@@ -1,10 +1,11 @@
 package Basiclevel.lesson1;
 
-import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,6 +14,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button citySelect = (Button) findViewById(R.id.button);
+        citySelect.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, CitySelection.class);
+                startActivity(intent);
+            }
+        });
+
     }
 
 }
